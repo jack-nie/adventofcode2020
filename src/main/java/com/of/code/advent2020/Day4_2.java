6ed3de1841ff1b2427f577328b2904cc0f3023bf
jack-nie <hust.ud.hui@gmail.com>
2020-12-26 16:52:55 +0800

    format code

--- src/main/java/com/of/code/advent2020/Day4_2.java
+++ src/main/java/com/of/code/advent2020/Day4_2.java
@@ -13,74 +13,74 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Day4_2 {
-	public static void main(String[] args) {
-		Path path;
-		try {
-			path = Paths.get(Day4_2.class.getResource("/day4.txt").toURI());
-			String[] strArray = Files.readString(path).split("\r\n\r\n");
-			long l = Arrays.stream(strArray).map(item -> {
+    public static void main(String[] args) {
+        Path path;
+        try {
+            path = Paths.get(Day4_2.class.getResource("/day4.txt").toURI());
+            String[] strArray = Files.readString(path).split("\r\n\r\n");
+            long l = Arrays.stream(strArray).map(item -> {
 
-				Map<String, String> map = Arrays.stream(item.split("\r\n"))
-						.flatMap(strs -> Arrays.stream(strs.split(" "))).map(str -> str.split(":"))
-						.collect(Collectors.toMap(array -> array[0], array -> array[1], (o1, o2) -> o2));
+                Map<String, String> map = Arrays.stream(item.split("\r\n"))
+                        .flatMap(strs -> Arrays.stream(strs.split(" "))).map(str -> str.split(":"))
+                        .collect(Collectors.toMap(array -> array[0], array -> array[1], (o1, o2) -> o2));
 
-				return map;
+                return map;
 
-			}).
-					filter(item -> Day4_1.validPassport(item.keySet().stream().collect(Collectors.toSet()))).
-					filter(item -> item.entrySet().stream().allMatch(map -> checkValidValue(map.getKey(), map.getValue()))).
-					count();
-			System.out.println(l);
-		} catch (IOException | URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+            }).
+                    filter(item -> Day4_1.validPassport(item.keySet().stream().collect(Collectors.toSet()))).
+                    filter(item -> item.entrySet().stream().allMatch(map -> checkValidValue(map.getKey(), map.getValue()))).
+                    count();
+            System.out.println(l);
+        } catch (IOException | URISyntaxException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
 
-	}
+    }
 
-	private static boolean checkInRange(String value, int begin, int end) {
-		int number = Integer.parseInt(value);
-		if (number >= begin && number <= end) {
-			return true;
-		}
-		return false;
-	}
+    private static boolean checkInRange(String value, int begin, int end) {
+        int number = Integer.parseInt(value);
+        if (number >= begin && number <= end) {
+            return true;
+        }
+        return false;
+    }
 
-	private static boolean checkValidValue(String key, String value) {
-		boolean flag = false;
+    private static boolean checkValidValue(String key, String value) {
+        boolean flag = false;
 
-		switch (key) {
-		case "byr":
-			flag = value.matches("^\\d{4}$") && checkInRange(value, 1920, 2002);
-			break;
-		case "iyr":
-			flag = value.matches("^\\d{4}$") && checkInRange(value, 2010, 2020);
-			break;
-		case "eyr":
-			flag = value.matches("^\\d{4}$") && checkInRange(value, 2020, 2030);
-			break;
-		case "hgt":
-			flag = value.matches("^\\d+cm$") && checkInRange(value.split("cm")[0], 150, 193)
-					|| value.matches("^\\d+in$") && checkInRange(value.split("in")[0], 59, 76);
-			break;
-		case "hcl":
-			flag = value.matches("^#[0-9a-f]{6}$");
-			break;
-		case "ecl":
-			flag = value.matches("^amb|blu|brn|gry|grn|hzl|oth$");
-			break;
-		case "pid":
-			flag = value.matches("^\\d{9}$");
-			break;
-		case "cid":
-			flag = true;
-			break;
-		default:
-			break;
-		}
+        switch (key) {
+            case "byr":
+                flag = value.matches("^\\d{4}$") && checkInRange(value, 1920, 2002);
+                break;
+            case "iyr":
+                flag = value.matches("^\\d{4}$") && checkInRange(value, 2010, 2020);
+                break;
+            case "eyr":
+                flag = value.matches("^\\d{4}$") && checkInRange(value, 2020, 2030);
+                break;
+            case "hgt":
+                flag = value.matches("^\\d+cm$") && checkInRange(value.split("cm")[0], 150, 193)
+                        || value.matches("^\\d+in$") && checkInRange(value.split("in")[0], 59, 76);
+                break;
+            case "hcl":
+                flag = value.matches("^#[0-9a-f]{6}$");
+                break;
+            case "ecl":
+                flag = value.matches("^amb|blu|brn|gry|grn|hzl|oth$");
+                break;
+            case "pid":
+                flag = value.matches("^\\d{9}$");
+                break;
+            case "cid":
+                flag = true;
+                break;
+            default:
+                break;
+        }
 
-		return flag;
+        return flag;
 
-	}
+    }
 
 }
--- src/main/java/com/of/code/advent2020/Day6_2.java
+++ src/main/java/com/of/code/advent2020/Day6_2.java
@@ -13,44 +13,44 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day6_2 {
-	public static void main(String[] args) {
-		Path path;
-		try {
-			path = Paths.get(Day6_2.class.getResource("/day6.txt").toURI());
-			String[] source = Files.readString(path).split("\n\n");
+    public static void main(String[] args) {
+        Path path;
+        try {
+            path = Paths.get(Day6_2.class.getResource("/day6.txt").toURI());
+            String[] source = Files.readString(path).split("\n\n");
 
-			int x = Arrays.stream(source).map(item -> commonCharsSize(item.split("\n"))).reduce((a, b) -> a + b).get();
+            int x = Arrays.stream(source).map(item -> commonCharsSize(item.split("\n"))).reduce((a, b) -> a + b).get();
 
-			System.out.println(x);
+            System.out.println(x);
 
-		} catch (IOException | URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+        } catch (IOException | URISyntaxException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
 
-	}
+    }
 
-	public static int commonCharsSize(String[] strings) {
-			List<String> result = new ArrayList<String>();
-			int[] count = new int[26];
-			for (int i=0; i<26; i++) {
-				count[i] = Integer.MAX_VALUE;
-			}
-			for (String str : strings) {
-				int[] tem = new int[26];
-				for (int j=0; j<str.length(); j++) {
-					tem[str.charAt(j)-'a']++;
-				}
-				for (int k=0; k<26; k++) {
-					count[k] = Math.min(count[k], tem[k]);
-				}
-			}
-			for (int i=0; i<26; i++) {
-				while (count[i]-- > 0) {
-					result.add((char)(i+'a')+"");
-				}
-			}
-			return result.size();
-	}
+    public static int commonCharsSize(String[] strings) {
+        List<String> result = new ArrayList<String>();
+        int[] count = new int[26];
+        for (int i = 0; i < 26; i++) {
+            count[i] = Integer.MAX_VALUE;
+        }
+        for (String str : strings) {
+            int[] tem = new int[26];
+            for (int j = 0; j < str.length(); j++) {
+                tem[str.charAt(j) - 'a']++;
+            }
+            for (int k = 0; k < 26; k++) {
+                count[k] = Math.min(count[k], tem[k]);
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            while (count[i]-- > 0) {
+                result.add((char) (i + 'a') + "");
+            }
+        }
+        return result.size();
+    }
 
 }
