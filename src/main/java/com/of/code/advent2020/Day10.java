b38d487472434072317355d863d8f387f0ae3592
jack-nie <hust.ud.hui@gmail.com>
2021-01-11 19:16:19 +0800

    format code

--- src/main/java/com/of/code/advent2020/Day10.java
+++ src/main/java/com/of/code/advent2020/Day10.java
@@ -1,6 +1,5 @@
 package com.of.code.advent2020;
 
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -14,37 +13,37 @@ public class Day10 {
 		Collections.sort(list);
 		int result = part1(list);
 		System.out.println(result);
-		
+
 		Long result2 = part2(list);
 		System.out.println(result2);
 	}
 
 	public static int part1(List<Long> list) {
 		HashMap<Long, Integer> hashMap = new HashMap<>();
-        
+
 		for (int i = 1; i < list.size(); i++) {
-			long diff = list.get(i) - list.get(i-1);
+			long diff = list.get(i) - list.get(i - 1);
 			hashMap.put(diff, hashMap.getOrDefault(diff, 0) + 1);
 		}
 
 		return hashMap.get(1L) * (hashMap.get(3L) + 1);
 	}
-	
+
 	public static Long part2(List<Long> list) {
 		HashMap<Long, Long> hashMap = new HashMap<>();
-		hashMap.put((long) 0,  1L);
-		List<Long> diffs = Arrays.asList(1L,2L,3L);
-		
-        for (int i = 1; i < list.size(); i++) {
-        	hashMap.put(list.get(i), 0L);
-        	for (Long diff:diffs) {
-        		if (hashMap.get(list.get(i) -diff) != null) {
-        			hashMap.put(list.get(i), hashMap.get(list.get(i)) + hashMap.get(list.get(i) - diff));
-        		}
-        	}
-        	
-        }
-        return hashMap.get(list.get(list.size() - 1));
+		hashMap.put((long) 0, 1L);
+		List<Long> diffs = Arrays.asList(1L, 2L, 3L);
+
+		for (int i = 1; i < list.size(); i++) {
+			hashMap.put(list.get(i), 0L);
+			for (Long diff : diffs) {
+				if (hashMap.get(list.get(i) - diff) != null) {
+					hashMap.put(list.get(i), hashMap.get(list.get(i)) + hashMap.get(list.get(i) - diff));
+				}
+			}
+
+		}
+		return hashMap.get(list.get(list.size() - 1));
 	}
 
 	public static Long[] convertLinesToList(Stream<String> lines) {
