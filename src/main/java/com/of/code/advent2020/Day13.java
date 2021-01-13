93634976becbd8e83a564069e54bf19ad0b949bd
jack-nie <hust.ud.hui@gmail.com>
2021-01-13 15:55:53 +0800

    day 13

--- src/main/java/com/of/code/advent2020/Day13.java
+++ src/main/java/com/of/code/advent2020/Day13.java
@@ -1,5 +1,9 @@
 package com.of.code.advent2020;
 
+import static java.lang.Long.parseLong;
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -11,6 +15,9 @@ public class Day13 {
 		List<String> lines = Helper.generateStringArrayFromFile("/day13.txt");
 		long part1 = part1(lines);
 		System.out.println(part1);
+		
+		long part2 = part2(lines);
+		System.out.println(part2);
 
 	}
 
@@ -38,6 +45,23 @@ public class Day13 {
 
 		return pattern.matcher(str).matches();
 
+	}
+
+	public static long part2(List<String> lines) {
+		String[] s = lines.get(1).split(",");
+		long[][] nums = range(0, s.length).filter(i -> !s[i].equals("x"))
+				.mapToObj(i -> new long[] { parseLong(s[i]), i }).toArray(long[][]::new);
+		long product = stream(nums).mapToLong(a -> a[0]).reduce((a, b) -> a * b).getAsLong();
+		long sum = stream(nums).mapToLong(a -> a[1] * (product / a[0]) * inverseModulo(product / a[0], a[0])).sum();
+		return product - sum % product;
+	}
+
+	public static long inverseModulo(long x, long y) {
+		if (x != 0) {
+			long modulo = y % x;
+			return modulo == 0 ? 1 : y - inverseModulo(modulo, x) * y / x;
+		}
+		return 0;
 	}
 
 }
